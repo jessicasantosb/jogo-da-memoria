5b01f2d16e4b29ed02de3fcb12b3ba83c4d987ec
jessica <jessica.santosb@outlook.com>
2024-04-04 14:23:06 -0300

    Feat: login error message

--- js/login.js
+++ js/login.js
@@ -2,26 +2,34 @@ const input = document.querySelector(".login__input");
 const button = document.querySelector(".login__button");
 const form = document.querySelector(".login");
 const amount = document.querySelector(".login__amount");
+const error = document.querySelector(".login__error");
 let pars = "";
+let nameChecked = false;
+let buttonChecked = false;
 
 input.addEventListener("input", ({ target }) => {
-  if (target.value.length > 3) {
-    button.removeAttribute("disabled");
+  if (target.value.length > 2 && target.value.length <= 10) {
+    error.classList.add("hide");
+    if (buttonChecked) button.removeAttribute("disabled");
     return;
+  } else {
+    error.classList.remove("hide");
+    button.setAttribute("disabled", "");
   }
-  button.setAttribute("disabled", "");
 });
 
 amount.addEventListener("click", ({ target }) => {
   const isButton = target.tagName === "BUTTON";
 
   if (isButton) {
-    const buttons = document.querySelectorAll(".login__amount-num")
+    const buttons = document.querySelectorAll(".login__amount-num");
     pars = target.value;
-    buttons.forEach(button => {
+    buttons.forEach((button) => {
       button.classList.remove("active");
+      buttonChecked = true;
     });
     target.classList.add("active");
+    if (nameChecked) button.removeAttribute("disabled");
   }
 });
 
